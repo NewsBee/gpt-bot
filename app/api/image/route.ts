02e75d93b9a6366631a9c523bbc470dba1a40888
NewsBee13 <mstrdzaki@gmail.com>
2023-11-08 19:36:42 +0700

    api image

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,3 +1,4 @@
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
@@ -32,12 +33,22 @@ export async function POST(
       return new NextResponse("Resolution is required", { status: 400 });
     }
 
+    const freeTrial = await checkApiLimit()
+
+    if(!freeTrial){
+      return new NextResponse("Free trial has expired.", { status:403 })
+    }
+
+
 
     const response = await openai.images.generate({
       prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+
+    await increaseApiLimit()
+
 
 
     return NextResponse.json(response.data);
